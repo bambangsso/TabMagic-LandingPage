84e22f557e90444cbd7cb50492dfe6dc4d44bcc5
Bambang Susilo <bambangsusilo@Bambangs-MacBook-Air.local>
2024-12-10 22:09:06 +0700

    add new tabmagic logo

--- src/components/TabMagicLogo.tsx
+++ src/components/TabMagicLogo.tsx
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { Layout, Layers } from 'lucide-react';
 
@@ -7,5 +8,20 @@ export function TabMagicLogo() {
       <Layers className="h-8 w-8 text-blue-600" />
       <Layout className="h-4 w-4 text-blue-400 absolute -bottom-1 -right-1" />
     </div>
+  );
+}
+*/
+
+import React from 'react';
+import tabMagicLogo from '../assets/tabmagic-logo-web.png';
+
+export function TabMagicLogo() {
+  return (
+    <img 
+      src={tabMagicLogo} 
+      alt="TabMagic Logo" 
+      className="h-8 w-8"
+      style={{ objectFit: 'contain' }}
+    />
   );
 }
